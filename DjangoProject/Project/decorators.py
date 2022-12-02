dac9a2eb52522e2e785d84ef15cdfad5345b0d04
Anshul Dhariwal <anshuldhariwal04@gmail.com>
2022-12-01 23:49:50 -0800

    Tags

--- DjangoProject/Project/decorators.py
+++ DjangoProject/Project/decorators.py
@@ -1,13 +1,17 @@
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+from .models import OrgGroups
 
 
-def user_is_entry_author(function):
+def registered_org_user(function):
     def wrap(request, *args, **kwargs):
-        entry = Entry.objects.get(pk=kwargs['entry_id'])
-        if entry.created_by == request.user:
-            return function(request, *args, **kwargs)
+        if request is not None and request.user.is_authenticated:
+            if OrgGroups.objects.filter(user_id=request.user.id).exists():
+                return function(request, *args, **kwargs)
+            else:
+                raise PermissionDenied
         else:
-            raise PermissionDenied
+            return redirect("/login")
 
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
